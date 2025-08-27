61c698b9aa87bfd0a8541b4c211c944abe60ee21
Keven - cc23143 <cc23143@g.unicamp.br>
2025-08-27 18:10:01 -0300

    Audio Listener check
    Fazer os audios serem mais altos

--- Sistema Solar/Assets/Scripts/ChangeLookAtTarget.cs
+++ Sistema Solar/Assets/Scripts/ChangeLookAtTarget.cs
@@ -4,19 +4,29 @@ using System.Collections;
 public class ChangeLookAtTarget : MonoBehaviour {
 
 	public GameObject target; // the target that the camera should look at
+	private AudioSource backgroundAudio;
 
-	void Start() {
-		if (target == null) 
+	void Start()
+	{
+		if (target == null)
 		{
 			target = this.gameObject;
-			Debug.Log ("ChangeLookAtTarget target not specified. Defaulting to parent GameObject");
+			Debug.Log("ChangeLookAtTarget target not specified. Defaulting to parent GameObject");
+		}
+		backgroundAudio = GetComponent<AudioSource>();
+		if (backgroundAudio == null)
+		{
+			Debug.Log($"O componente {gameObject.name} não tem audio source.");
 		}
 	}
 
 	// Called when MouseDown on this gameObject
 	void OnMouseDown () {
-		// change the target of the LookAtTarget script to be this gameobject.
 		LookAtTarget.target = target;
+		if (backgroundAudio != null)
+		{
+			LookAtTarget.currentAudio = backgroundAudio;
+		}
 		Camera.main.fieldOfView = 60*target.transform.localScale.x;
 	}
 }
--- Sistema Solar/Assets/Scripts/LookAtTarget.cs
+++ Sistema Solar/Assets/Scripts/LookAtTarget.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class LookAtTarget : MonoBehaviour {
 
 	static public GameObject target; // the target that the camera should look at
+	static public AudioSource currentAudio;
+	private AudioSource pastAudio = null;
 
 	void Start () {
 		if (target == null) 
@@ -12,10 +14,21 @@ public class LookAtTarget : MonoBehaviour {
 			Debug.Log ("LookAtTarget target not specified. Defaulting to parent GameObject");
 		}
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
+	void Update()
+	{
 		if (target)
 			transform.LookAt(target.transform);
+		if (currentAudio != pastAudio)
+		{
+			if (pastAudio != null)
+			{
+				pastAudio.mute = true;
+			}
+			Debug.Log($"tocando {currentAudio.name}");
+			pastAudio = currentAudio;
+			currentAudio.mute = false;
+		}
 	}
 }
